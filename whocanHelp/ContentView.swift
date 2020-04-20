107cbe9f9a1ce13aaaa06c2ec3ea39fbe89d00e4
yujenpan <whiper66065@gmail.com>
2020-04-20 20:35:32 +0800

    Initial Commit

---
+++ whocanHelp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  whocanHelp
+//
+//  Created by 潘昱任 on 2020/4/20.
+//  Copyright © 2020 ntouyujen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
